cba269955756fbcf1bb4d35dbec6573da46e0a92
hue-death <mohamedyehya866@gmail.com>
2026-08-17 16:46:24 +0300

    nothing
    a

--- js/layers.js
+++ js/layers.js
@@ -26,3 +26,10 @@ addLayer("p", {
     ],
     layerShown(){return true}
 })
+upgrades: {
+    11: {
+        name: "an upgrade name",
+        description: "Multiply your points",
+        cost: new Decimal(1)
+    }
+}
