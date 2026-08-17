447d697ad67e902f9deed1d3b120efa91c66b25e
hue-death <mohamedyehya866@gmail.com>
2026-08-17 17:06:08 +0300

    Update

--- js/layers.js
+++ js/layers.js
@@ -24,12 +24,12 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            name: "an upgrade name",
+            description: "Multiply your points",
+            cost: new Decimal(1)
+        },
+    }
 })
-upgrades: {
-    
-        name: "an upgrade name"
-        description: "Multiply your points"
-        cost: new Decimal(1)
-    
-}
